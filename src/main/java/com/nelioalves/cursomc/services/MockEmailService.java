4b9cb2976899bfd4899084efbf0c49f06a9641ca
pikeno221 <gabriellgomesbh@gmail.com>
2018-08-30 20:17:40 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,5 +16,14 @@ public class MockEmailService extends AbstractEmailService {
 		log.info(msg.toString());
 		log.info("Email enviado");
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Simulando envio de email html...");
+		log.info(msg.toString());
+		log.info("Email enviado");
+	}
+
+
 
 }
--- src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
@@ -1,15 +1,22 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javamailSender;
+	
 	
 	private static final Logger log = LoggerFactory.getLogger(SmtpEmailService.class);
 	@Override
@@ -19,5 +26,10 @@ public class SmtpEmailService extends AbstractEmailService {
 		log.info("Email enviado");
 		
 	}
-
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Enviando de email...");
+		javamailSender.send(msg);
+		log.info("Email enviado");
+	}
 }
